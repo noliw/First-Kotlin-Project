61177122cf5b381b76f401ad941d46127f4d233e
noliw <noliw2017@gmail.com>
2023-03-01 20:34:25 -0800

    Initial commit

---
+++ app/src/main/java/com/codecademy/asciiart/main.kt
@@ -0,0 +1,13 @@
+package com.codecademy.asciiart
+
+fun main() {
+    println("     .....      ")
+    println("  .         .   ")
+    println(" .   0  0    .  ")
+    println("  ...........   ")
+    println("   .. __ ..     ")
+    println("  . .|C_|. .    ")
+    println(" .  ......  .   ")
+    println("     .  .       ")
+    println("     .  .       ")
+}
